8ea7652b301a40726a300150d18a03db2ebdd52e
mole828 <mrx800@163.com>
2023-07-20 18:29:19 +0800

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        ProcessThrust();
+    }
+
+    public float rotateForce = 100f;
+    public float thrustForce = 100f;
+    private void Rotated(float force)
+    {
+        _rigidbody.constraints ^= RigidbodyConstraints.FreezeRotationZ;
+        transform.Rotate(force * Time.deltaTime * Vector3.forward);
+        _rigidbody.constraints ^= RigidbodyConstraints.FreezeRotationZ;
+    }
+    private void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidbody.AddRelativeForce(thrustForce * Time.deltaTime * Vector3.up );
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Rotated(rotateForce);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Rotated(-rotateForce);
+        }
+    }
+}
